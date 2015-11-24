ef140ebd5d8be6213f15e38ca07818b8f37069e9
Cedric <cedric.lor@gmail.com>
2015-11-24 02:04:14 +0100

    cleaned up the lightbox

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+  // if they click on a boxed image, enlarge it
+  $('div.img-box img').click(function(event) {
+    $(this).removeClass("img-belittled").addClass("img-enlarged").closest('div.img-box').addClass("lightbox");
+    // if they click an enlarged image, close it
+  });
+  $('body').on("click", ".lightbox", function(event) {
+    console.log($(".active-lightbox").length);
+    if ($(".active-lightbox").length > 0) {
+      $('div.img-box img').addClass("img-belittled").removeClass("img-enlarged").closest('div.img-box').removeClass("lightbox");
+      $(".active-lightbox").removeClass("active-lightbox");
+      return;
+    } else {
+      $('div.img-box').addClass("active-lightbox");
+    }
+  });
 });
